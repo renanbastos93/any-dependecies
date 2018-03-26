f4a3a42a5898ca0d1d1020799bc032457c68bc1b
RENAN.BASTOS <renanbastos.tec@gmail.com>
2018-03-26 17:39:49 -0300

    Update installDependecies.sh

--- installDependecies.sh
+++ installDependecies.sh
@@ -2,25 +2,22 @@
 
 FILE=$1
 LANGUAGE=$2
-MYPATH="/git"
-
-function test {
-    echo "test run"
-}
+MYPATH="/mnt/c/projetos"
 
 function installNodejsDependecies {
     npm install >> ../nodejs-dependecies-log.txt
 }
 
 case $LANGUAGE in
-    "node")
+    "node" )
         for DIR in $(ls -d $pasta*/); do
             cd $MYPATH"/"$DIR
             if [ -f $FILE ]; then
-                echo "Open ". $DIR >> ../nodejs-dependecies-log.txt
+                echo "Open "$DIR >> ../nodejs-dependecies-log.txt
                 installNodejsDependecies
-                echo "Install success ".$DIR
-                echo "Close of path ".$DIR >> ../nodejs-dependecies-log.txt
+                echo "Install success "$DIR
+                echo "Install success "$DIR | >> ../nodejs-dependecies-log.txt
+                echo "Close of path "$DIR | >> ../nodejs-dependecies-log.txt
                 cd $MYPATH
             fi
         done
